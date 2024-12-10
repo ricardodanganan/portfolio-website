ad6f3a1f0ab102dd4549417507f8d6ff1fd47e88
Ricardo Danganan Jnr <x00191395@mytudublin.ie>
2024-12-10 21:13:16 +0000

    add the fixed top to the navbar as an alternative to sticky navbar feature

--- js/navbar.js
+++ js/navbar.js
@@ -2,15 +2,11 @@ const menuHolder = document.getElementById("menuHolder");
 const menuDrawer = document.getElementById("menuDrawer");
 const siteBrand = document.getElementById("siteBrand");
 
-function menuToggle() {
+function navbarMenuToggle() {
+  const menuDrawer = document.getElementById("navbar-menuDrawer");
   if (menuDrawer.style.transform === "translateX(0%)") {
-    menuDrawer.style.transform = "translateX(-100%)";
-    menuHolder.classList.add("sticky-top");
+      menuDrawer.style.transform = "translateX(-100%)";
   } else {
-    menuDrawer.style.transform = "translateX(0%)";
-    menuHolder.classList.remove("sticky-top");
+      menuDrawer.style.transform = "translateX(0%)";
   }
 }
-
-
-
