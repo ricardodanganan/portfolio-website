ac99a66be3f55da2e102c033b15e8f1644913650
Ricardo Danganan Jnr <x00191395@mytudublin.ie>
2024-12-10 21:44:06 +0000

    added a feature on the menu drawer that closes it when clicked anywhere on the site

--- js/navbar.js
+++ js/navbar.js
@@ -4,9 +4,21 @@ const siteBrand = document.getElementById("siteBrand");
 
 function navbarMenuToggle() {
   const menuDrawer = document.getElementById("navbar-menuDrawer");
+  const overlay = document.getElementById("navbar-overlay");
+
   if (menuDrawer.style.transform === "translateX(0%)") {
       menuDrawer.style.transform = "translateX(-100%)";
+      overlay.style.display = "none"; // Hide overlay
   } else {
       menuDrawer.style.transform = "translateX(0%)";
+      overlay.style.display = "block"; // Show overlay
   }
+}
+
+function closeMenu() {
+  const menuDrawer = document.getElementById("navbar-menuDrawer");
+  const overlay = document.getElementById("navbar-overlay");
+
+  menuDrawer.style.transform = "translateX(-100%)";
+  overlay.style.display = "none"; // Hide overlay
 }
